793613e61de04231dc473a3fe4f8a5921418e08d
Sarath TS <sarathiscookie@hotmail.com>
2019-05-03 13:01:58 +0200

    First commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone', 20)->nullable();
+            $table->string('street', 150)->nullable();
+            $table->string('postal', 50)->nullable();
+            $table->string('city', 150)->nullable();
+            $table->string('country', 150)->nullable();
+            $table->enum('active', ['yes', 'no'])->default('no');
+            $table->enum('user_type', ['admin', 'manager', 'employee']);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
