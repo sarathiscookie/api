ff0d118785d8c6d6c71b740dab2c3cba4c0a3183
Sarath TS <sarathiscookie@hotmail.com>
2019-05-09 14:29:35 +0200

    Admin: Added model in datatables to show user details

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -50,39 +50,17 @@ class ManagerController extends Controller
 
         // Search query for email
         if(!empty($request->input('search.value'))) {
-            $search   = $request->input('search.value');
-            $q->where(function($query) use ($search) {
-                $query->where('email', 'like', "%{$search}%");
-            });
-
-           $totalFiltered = $q->where(function($query) use ($search) {
-            $query->where('email', 'like', "%{$search}%");
-           })
-           ->count();
+            $this->searchUserEmail($q, $request->input('search.value'));
         }
 
         // tfoot search query for email
         if( !empty($params['columns'][2]['search']['value']) ) {
-            $q->where(function($query) use ($params) {
-                $query->where('email', 'like', "%{$params['columns'][2]['search']['value']}%");
-            });
-
-            $totalFiltered = $q->where(function($query) use ($params) {
-                $query->where('email', 'like', "%{$params['columns'][2]['search']['value']}%");
-            })
-            ->count();
+            $this->tfootUserEmail($q, $params['columns'][2]['search']['value']);
         }
 
         // tfoot search query for user status
         if( !empty($params['columns'][4]['search']['value']) ) {
-            $q->where(function($query) use ($params) {
-                $query->where('active', "{$params['columns'][4]['search']['value']}");
-            });
-
-            $totalFiltered = $q->where(function($query) use ($params) {
-                $query->where('active', "{$params['columns'][4]['search']['value']}");
-            })
-            ->count();
+            $this->tfootUserStatus($q, $params['columns'][4]['search']['value']);
         }
 
         $managerLists = $q->skip($start)
@@ -96,7 +74,7 @@ class ManagerController extends Controller
             foreach ($managerLists as $key=> $managerList) {
                 $nestedData['hash']       = '<input class="checked" type="checkbox" name="id[]" value="'.$managerList->id.'" />';
                 $nestedData['name']       = $managerList->name;
-                $nestedData['email']      = $managerList->email;
+                $nestedData['email']      = $this->bootstrapModal($managerList->id, $managerList->email, $managerList->phone, $managerList->street, $managerList->postal, $managerList->city, $managerList->country);
                 $nestedData['created_at'] = date('d.m.y', strtotime($managerList->created_at));
                 $nestedData['active']     = $managerList->active;
                 $nestedData['actions']    = '<a type="button" class="btn btn-secondary btn-sm"><i class="fas fa-user-edit"></i></a> <a type="button" class="btn btn-secondary btn-sm deleteEvent" data-id="'.$managerList->id.'"><i class="fas fa-trash-alt"></i></a>';
@@ -112,6 +90,129 @@ class ManagerController extends Controller
         );
 
         return response()->json($json_data);
+    }
+
+    /**
+     * Search query for user email
+     * @param  string $q
+     * @param  string $searchData
+     * @return \Illuminate\Http\Response
+     */
+    public function searchUserEmail($q, $searchData)
+    {
+        $q->where(function($query) use ($searchData) {
+            $query->where('email', 'like', "%{$searchData}%");
+        });
+
+        $totalFiltered = $q->where(function($query) use ($searchData) {
+            $query->where('email', 'like', "%{$searchData}%");
+        })
+        ->count();
+
+        return $this;    
+    }
+
+    /**
+     * tfoot search query for user email
+     * @param  string $q
+     * @param  string $searchData
+     * @return \Illuminate\Http\Response
+     */
+    public function tfootUserEmail($q, $searchData)
+    {
+        $q->where(function($query) use ($searchData) {
+            $query->where('email', 'like', "%{$searchData}%");
+        });
+
+        $totalFiltered = $q->where(function($query) use ($searchData) {
+            $query->where('email', 'like', "%{$searchData}%");
+        })
+        ->count();
+
+        return $this;    
+    }
+
+    /**
+     * tfoot search query for user status
+     * @param  string $q
+     * @param  string $searchData
+     * @return \Illuminate\Http\Response
+     */
+    public function tfootUserStatus($q, $searchData)
+    {
+        $q->where(function($query) use ($searchData) {
+            $query->where('active', "{$searchData}");
+        });
+
+        $totalFiltered = $q->where(function($query) use ($searchData) {
+            $query->where('active', "{$searchData}");
+        })
+        ->count();
+
+        return $this;    
+    }
+
+    /**
+     * Bootstrap model for datatable
+     * @param  string $userId
+     * @return \Illuminate\Http\Response
+     */
+    public function bootstrapModal($userId, $email, $phone, $street, $postal, $city, $country)
+    {
+        $html = '<a href="" data-toggle="modal" data-target="#userModal_'.$userId.'">
+        '.$email.'
+        </a>
+        <div class="modal fade" id="userModal_'.$userId.'" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="modalLabel">Manager Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+        <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Phone</h5>
+        </div>
+        <p class="mb-1">'.$phone.'</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Street</h5>
+        </div>
+        <p class="mb-1">'.$street.'</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Postal</h5>
+        </div>
+        <p class="mb-1">'.$postal.'</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">City</h5>
+        </div>
+        <p class="mb-1">'.$city.'</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">Country</h5>
+        </div>
+        <p class="mb-1">'.$country.'</p>
+        </a>
+        </div>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+        </div>
+        </div>';
+
+        return $html;
     }
 
     /**
