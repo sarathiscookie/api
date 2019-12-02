23c7d72557c5847a550315fab4f0683f7830ff7e
sarathts <sarathiscookie@hotmail.com>
2019-12-02 14:41:29 +0530

    Admin: Order List - Clean up UI

--- resources/views/Admin/order.blade.php
+++ resources/views/Admin/order.blade.php
@@ -20,31 +20,29 @@
         <div class="card-body">
 
           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputShop">Shop</label>
+            <div class="form-group col-md-3">
               <select id="inputShop" class="form-control">
                 <option>Choose shop</option>
                 <option>...</option>
               </select>
             </div>
 
-            <div class="form-group col-md-6">
-              <label for="inputCompany">Company</label>
+            <div class="form-group col-md-3">
               <select id="inputCompany" class="form-control">
                 <option>Choose company</option>
                 <option>...</option>
               </select>
             </div>
-          </div>
 
-          <div class="form-row">
-            <div class="form-group col-md-12">
-              <label for="dateRange">Daterange</label>
-              <input type="text" class="form-control" id="dateRange" name="dateRange">
+            <div class="form-group col-md-4">
+              <input type="text" class="form-control" id="dateRange" name="dateRange" placeholder="daterange">
+            </div>
+
+            <div class="form-group col-md-2">
+              <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Search Orders</button>
             </div>
           </div>
-
-          <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Search Orders</button>
+          <hr>
 
           <div class="text-right">
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createKeyModal"><i class="fas fa-download"></i> Download Invoices</button>
@@ -55,7 +53,7 @@
 
             <div class="orderResponseKeyMessage"></div>
 
-            <table id="order_list" class="table table-bordered table-hover display" style="width:100%">
+            <table id="" class="table table-bordered table-hover display" style="width:100%">
               <thead class="thead-light">
                 <tr>
                   <th>#</th>
